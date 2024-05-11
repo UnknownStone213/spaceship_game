e5f93fa0bce65308247af09836cc2164837ab59b
Vlad <vlad.khvesyukovich@gmail.com>
2024-05-11 15:19:59 +0300

    https://www.youtube.com/watch?v=R-u1EY9fOJQ&list=PL2wAo2qwCxGDp9fzBOTy_kpUTSwM1iWWd&index=2&t=1383

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+mod movement;
+mod debug;
+mod spaceship;
+
+use bevy::prelude::*;
+use debug::DebugPlugin;
+use movement::MovementPlugin;
+use spaceship::SpaceshipPlugin;
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_plugins(SpaceshipPlugin)
+        .add_plugins(MovementPlugin)
+        .add_plugins(DebugPlugin)
+        .run();
+}
