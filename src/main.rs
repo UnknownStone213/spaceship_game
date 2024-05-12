87b4165fccd78defe2ca90045191acb9603702e1
Vlad <vlad.khvesyukovich@gmail.com>
2024-05-12 14:09:01 +0300

    3

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,11 @@ mod movement;
 mod debug;
 mod spaceship;
 mod camera;
+mod asteroids;
+mod asset_loader;
 
+use asset_loader::AssetLoaderPlugin;
+use asteroids::AsteroidPlugin;
 use bevy::prelude::*;
 use camera::CameraPlugin;
 use debug::DebugPlugin;
@@ -17,9 +21,11 @@ fn main() {
             brightness: 0.75,
         })
         .add_plugins(DefaultPlugins)
-        .add_plugins(SpaceshipPlugin)
-        .add_plugins(CameraPlugin)
+        .add_plugins(AssetLoaderPlugin)
         .add_plugins(MovementPlugin)
         .add_plugins(DebugPlugin)
+        .add_plugins(SpaceshipPlugin)
+        .add_plugins(AsteroidPlugin)
+        .add_plugins(CameraPlugin)
         .run();
 }
--- src/spaceship.rs
+++ src/spaceship.rs
@@ -1,33 +1,44 @@
-use bevy::prelude::*;
+use bevy::{input::keyboard::KeyboardInput, prelude::*};
 
-use crate::movement::Velocity;
+use crate::{
+    asset_loader::SceneAssets,
+    movement::{Acceleration, MovingObjectBundle, Velocity},
+};
 
 const STARTING_TRANSLATION: Vec3 = Vec3::new(0.0, 0.0, -20.0);
-const STARTING_VELOCITY: Vec3 = Vec3::new(0.0, 0.0, 1.0);
+const SPACESHIP_SPEED: f32 = 25.0;
+const SPACESHIP_ROTATION_SPED: f32 = 2.5;
+const SPACESHIP_ROLL_SPEED: f32 = 2.5;
 
-#[derive(Bundle)]
-struct SpaceshipBundle {
-  velocity: Velocity,
-  model: SceneBundle,
-}
+#[derive(Component, Debug)]
+pub struct Spaceship;
 
 pub struct SpaceshipPlugin;
 
 impl Plugin for SpaceshipPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Startup, spawn_spaceship);
+        app.add_systems(PostStartup, spawn_spaceship);
     }
 }
 
-fn spawn_spaceship(mut commands: Commands, asset_server: Res<AssetServer>) {
-  commands.spawn(SpaceshipBundle {
-    velocity: Velocity{
-      value: STARTING_VELOCITY,
-    },
-    model: SceneBundle {
-      scene: asset_server.load("Spaceship.glb#Scene0"),
-      transform: Transform::from_translation(STARTING_TRANSLATION),
-      ..default()
-    },
-  });
+fn spawn_spaceship(mut commands: Commands, scene_assets: Res<SceneAssets>) {
+    commands.spawn((
+        MovingObjectBundle {
+            velocity: Velocity::new(Vec3::ZERO),
+            acceleration: Acceleration::new(Vec3::ZERO),
+            model: SceneBundle {
+                scene: scene_assets.spaceship.clone(),
+                transform: Transform::from_translation(STARTING_TRANSLATION),
+                ..default()
+            },
+        },
+        Spaceship,
+    ));
+}
+
+fn spaceship_movement_controls(
+    mut query: Query<(&mut Transform, &mut Velocity), With<Spaceship>>,
+    keyboard_input: Res<Input<KeyCode>>,
+    time: Res<Time>,
+) {
 }
